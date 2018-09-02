d3d0c818a8431bbb45b173d788d747faea2b1d6f
Piezo_ <orderofthefourthwall@gmail.com>
2018-09-02 23:59:41 +0000

    Update 'init.lua'

--- init.lua
+++ init.lua
@@ -102,8 +102,7 @@ minetest.register_globalstep(function(dtime)
 					text = player_name,
 					color = tagColor
 				})
-				better_nametags.players[player_name] = true
-				--I would use set_attach(), but it causes a glitch where the entity's nametag is stuck at (0,0,0) when viewed by the attached player
+				better_nametags.players[player_name] = true --I would use set_attach(), but it causes a glitch where the entity's nametag is stuck at (0,0,0) when viewed by the attached player
 			end
 			
 			if player:get_player_control().sneak and better_nametags.allowSneak then
@@ -114,4 +113,4 @@ minetest.register_globalstep(function(dtime)
 			end
 		end
 	end
-end
+end)
